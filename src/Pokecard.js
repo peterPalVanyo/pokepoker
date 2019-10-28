ceaabb8198d1e2c259db8ff64af6cbd9eb29beef
peterPalVanyo <vanyo.peter.pal@gmail.com>
2019-10-28 11:36:39 +0100

    refactor the 3digit function

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react';
 import './Pokecard.css';
 const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/"
 
+let threeDigit = (number) => (number < 1000 ? `00${number}`.slice(-3) : number)
+
 class Pokecard extends Component{
      render() {
         const id = idChecker(this.props.id)
         return (
             <div className='Pokecard'>
                 <h1 className='Pokecard-title'>{this.props.name}</h1>
-                <img src={`${POKE_API}${id}.png`} alt='pokemon'/>
+                <img src={`${POKE_API}${threeDigit(this.props.id)}.png`} alt='pokemon'/>
                 <div className='Pokecard-data'>Type: {this.props.type}</div>
                 <div className='Pokecard-data'>EXP: {this.props.exp}</div>
 
